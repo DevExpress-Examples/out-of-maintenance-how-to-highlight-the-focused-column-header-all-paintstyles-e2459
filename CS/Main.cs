25dcf6d0b7ae7beab82e04ea478b18c80a5ddefd
SvetlanaPN <26325291+SvetlanaPN@users.noreply.github.com>
2019-02-12 14:37:21 +0300

    remove dataset

--- CS/Main.cs
+++ CS/Main.cs
@@ -1,3 +1,6 @@
+using DevExpress.Utils.Drawing;
+using DevExpress.XtraEditors;
+using DevExpress.XtraGrid.Views.BandedGrid;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -6,32 +9,32 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 
-namespace WindowsApplication1
+namespace DXSample
 {
-    public partial class Form1 : Form
+    public partial class Main : XtraForm
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            // TODO: This line of code loads data into the 'nwindDataSet.Customers' table. You can move, or remove it, as needed.
-            this.customersTableAdapter.Fill(this.nwindDataSet.Customers);
-
+            recordBindingSource.DataSource = DataHelper.GetData(10);
         }
 
         private void advBandedGridView1_FocusedColumnChanged(object sender, DevExpress.XtraGrid.Views.Base.FocusedColumnChangedEventArgs e)
         {
-            advBandedGridView1.InvalidateColumnHeader(e.PrevFocusedColumn);
-            advBandedGridView1.InvalidateColumnHeader(e.FocusedColumn);
+            AdvBandedGridView view = sender as AdvBandedGridView;
+            view.InvalidateColumnHeader(e.PrevFocusedColumn);
+            view.InvalidateColumnHeader(e.FocusedColumn);
         }
 
         private void advBandedGridView1_CustomDrawColumnHeader(object sender, DevExpress.XtraGrid.Views.Grid.ColumnHeaderCustomDrawEventArgs e)
         {
-            if (e.Column == advBandedGridView1.FocusedColumn)
-                e.Info.State = DevExpress.Utils.Drawing.ObjectState.Hot;
+            AdvBandedGridView view = sender as AdvBandedGridView;
+            if (e.Column == view.FocusedColumn)
+                e.Info.State = ObjectState.Hot;
         }
     }
 }
